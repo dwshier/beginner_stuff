4a57b7a9696215d38dbe2a7902fb373fa925cae5
unholy7 <101910772+unholy7@users.noreply.github.com>
2022-08-22 06:53:23 -0400

    Add files via upload
    just a script to quickly check versions of common stuff

---
+++ version script.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+# Checks versions of libraries 
+
+# Python versions
+import sys
+print('Python: {}' .format(sys.version))
+
+# scipy
+import scipy
+print('scipy: {}' .format(scipy.__version__))
+
+# numpy
+import numpy
+print('numpy: {}' .format(numpy.__version__))
+
+# matplotlib
+import matplotlib
+print('matplotlib: {}' .format(matplotlib.__version__))
+
+# pandas
+import pandas
+print('pandas: {}' .format(pandas.__version__))
+
+# scikit-learn
+import sklearn
+print('sklearn:{}' .format(sklearn.__version__))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
